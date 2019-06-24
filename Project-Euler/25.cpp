7a1d7692e784e59737986c1a71f64ee27897c42c
Chirag Thakur <chirag11032000@gmail.com>
2019-06-24 17:46:55 +0530

    Solved P-25

---
+++ Project-Euler/25.cpp
@@ -0,0 +1,85 @@
+/* 
+chirag11032000
+Chirag Thakur
+*/
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+using ll = long long;
+using db = double;
+using pii = pair < int, int >;
+using pll = pair < ll, ll >;
+
+template < typename T > using OrderedSet = tree < T, null_type, less < T >, rb_tree_tag, tree_order_statistics_node_update >;
+template < typename T > using MinPriorityQueue = priority_queue < T, vector < T >, greater < T > >;
+
+#ifndef ONLINE_JUDGE
+#define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
+template < typename Arg1 >
+void __f(const char* name, Arg1&& arg1) {
+	cerr << name << " : " << arg1 << std::endl;
+}
+template < typename Arg1, typename... Args >
+void __f(const char* names, Arg1&& arg1, Args&&... args) {
+	const char* comma = strchr(names + 1, ',');
+	cerr.write(names, comma - names) << " : " << arg1 << " | ";
+	__f(comma + 1, args...);
+}
+#else
+#define trace(...)
+#endif
+
+#define eb emplace_back
+#define mp make_pair
+#define mt make_tuple
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define unique_sort(x) sort(all(x)), x.resize(distance(x.begin(), unique(all(x))))
+
+const db PI = acos(-1);
+const ll LINF = LLONG_MAX;
+const int INF = INT_MAX, MOD = 1e9 + 7, N = 1e3;
+
+void test_case();
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	test_case();
+	cerr << "Time taken: " << int((clock() * 1000.) / CLOCKS_PER_SEC) << "ms\n";
+	return 0;
+}
+
+void test_case() {
+	vector<vector<int>> fib(2, vector<int>(N));
+	fib[0][0] = fib[1][0] = 1;
+	int cnt = 3;
+	while (true) {
+		vector<int> nxt(N), carry(N + 1);
+		for (int dig = 0; dig < N; ++dig) {
+			int tot = carry[dig] + fib[0][dig] + fib[1][dig];
+			nxt[dig] = tot % 10;
+			carry[dig + 1] = tot / 10;
+		}
+		int digs = 0;
+		for (int i = 0; i < N; ++i) {
+			if (nxt[i]) {
+				digs = i + 1;
+			}
+		}
+		if (digs == N) {
+			cout << cnt << "\n";
+			return;
+		} else {
+			cnt++;
+			fib[0] = fib[1];
+			fib[1] = nxt;
+		}
+	}
+}
