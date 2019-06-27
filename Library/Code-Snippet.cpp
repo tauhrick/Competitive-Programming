1b4fe63ffaaad96c4ea8520ac8ec9266c9b8dc42
Chirag Thakur <chirag11032000@gmail.com>
2019-06-27 21:39:25 +0530

    Changed code-snippet

--- Library/Code-Snippet.cpp
+++ Library/Code-Snippet.cpp
@@ -1,61 +1,56 @@
-/* 
-chirag11032000
-Chirag Thakur
-*/
-
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-
 using namespace std;
-using namespace __gnu_pbds;
 
-using ll = long long;
-using db = double;
-using pii = pair < int, int >;
-using pll = pair < ll, ll >;
-
-template < typename T > using OrderedSet = tree < T, null_type, less < T >, rb_tree_tag, tree_order_statistics_node_update >;
-template < typename T > using MinPriorityQueue = priority_queue < T, vector < T >, greater < T > >;
-
-#ifndef ONLINE_JUDGE
-#define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
-template < typename Arg1 >
-void __f(const char* name, Arg1&& arg1) {
-	cerr << name << " : " << arg1 << std::endl;
+string to_string(string s) {
+	return '"' + s + '"';
 }
-template < typename Arg1, typename... Args >
-void __f(const char* names, Arg1&& arg1, Args&&... args) {
-	const char* comma = strchr(names + 1, ',');
-	cerr.write(names, comma - names) << " : " << arg1 << " | ";
-	__f(comma + 1, args...);
+
+string to_string(bool b) {
+	return (b ? "true" : "false");
 }
+
+template <typename A, typename B>
+string to_string(pair<A, B> p) {
+	return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+
+template <typename A>
+string to_string(A v) {
+	bool first = true;
+	string res = "{";
+	for (const auto &x : v) {
+		if (!first) {
+			res += ", ";
+		}
+		first = false;
+		res += to_string(x);
+	}
+	res += "}";
+	return res;
+}
+
+void debug_out() { cerr << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+	cerr << " " << to_string(H);
+	debug_out(T...);
+}
+
+#ifdef LOCAL
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
 #else
-#define trace(...)
+#define debug(...) 42
 #endif
 
-#define eb emplace_back
-#define mp make_pair
-#define mt make_tuple
-#define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
-#define unique_sort(x) sort(all(x)), x.resize(distance(x.begin(), unique(all(x))))
-
-const db PI = acos(-1);
-const ll LINF = LLONG_MAX;
-const int INF = INT_MAX, MOD = 1e9 + 7, N = 1e5 + 10;
-
-void test_case();
+void test_case() {
+	
+}
 
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
 	test_case();
-	cerr << "Time taken: " << int((clock() * 1000.) / CLOCKS_PER_SEC) << "ms\n";
 	return 0;
-}
-
-void test_case() {
-
 }
