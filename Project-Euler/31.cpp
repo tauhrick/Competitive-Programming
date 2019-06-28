acb989677129bc6a3fbc729b50aac67784e5ab90
Chirag Thakur <chirag11032000@gmail.com>
2019-06-29 05:04:41 +0530

    Solved P-31

---
+++ Project-Euler/31.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string to_string(string s) {
+	return '"' + s + '"';
+}
+
+string to_string(bool b) {
+	return (b ? "true" : "false");
+}
+
+template <typename A, typename B>
+string to_string(pair<A, B> p) {
+	return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+
+template <typename A>
+string to_string(A v) {
+	bool first = true;
+	string res = "{";
+	for (const auto &x : v) {
+		if (!first) {
+			res += ", ";
+		}
+		first = false;
+		res += to_string(x);
+	}
+	res += "}";
+	return res;
+}
+
+void debug_out() { cerr << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+	cerr << " " << to_string(H);
+	debug_out(T...);
+}
+
+#ifdef LOCAL
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+vector<int> v = {1, 2, 5, 10, 20, 50, 100, 200};
+int dp[10][210];
+
+int solve(int ind, int rem) {
+	if (rem < 0) {
+		return 0;
+	}
+	if (ind == v.size()) {
+		return rem == 0;
+	}
+	auto& ans = dp[ind][rem];
+	if (ans == -1) {
+		ans = 0;
+		for (int take = 0; take <= 200; ++take) {
+			ans += solve(ind + 1, rem - take * v[ind]);
+		}
+	}
+	return ans;
+}
+
+void test_case() {
+	memset(dp, -1, sizeof dp);
+	cout << solve(0, 200) << "\n";
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	test_case();
+	return 0;
+}
