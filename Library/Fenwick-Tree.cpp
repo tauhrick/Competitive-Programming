ee91a88c76ebef99af4adb72ab6bb2b9e6c23379
Chirag Thakur <chirag11032000@gmail.com>
2019-07-03 12:03:18 +0530

    Changed indentation to spaces

--- Library/Fenwick-Tree.cpp
+++ Library/Fenwick-Tree.cpp
@@ -1,26 +1,26 @@
 template <typename T>
 class Fenwick {
 public:
-	vector<T> Fenw;
-	int n;
+  vector<T> Fenw;
+  int n;
 
-	Fenwick(int _n) : n(_n) {
-		Fenw.rezize(n);
-	}
+  Fenwick(int _n) : n(_n) {
+    Fenw.rezize(n);
+  }
 
-	void modify(int x, T v) {
-		while (x < n) {
-			Fenw[x] += v;
-			x |= (x + 1);
-		}
-	}
+  void modify(int x, T v) {
+    while (x < n) {
+      Fenw[x] += v;
+      x |= (x + 1);
+    }
+  }
 
-	T get(int x) {
-		T v{};
-		while (x >= 0) {
-			v += Fenw[x];
-			x = (x & (x + 1)) - 1;
-		}
-		return v;
-	}
+  T get(int x) {
+    T v{};
+    while (x >= 0) {
+      v += Fenw[x];
+      x = (x & (x + 1)) - 1;
+    }
+    return v;
+  }
 };
--- Library/Modular.cpp
+++ Library/Modular.cpp
@@ -1,82 +1,82 @@
 template <uint32_t mod>
 class Modular {
 private:
-	uint32_t n;
+  uint32_t n;
 
 public:
-	Modular(int64_t _n = 0) : n((_n >= 0 ? _n : mod - (-_n) % mod) % mod) {}
+  Modular(int64_t _n = 0) : n((_n >= 0 ? _n : mod - (-_n) % mod) % mod) {}
 
-	uint32_t get() const {
-		return n;
-	}
+  uint32_t get() const {
+    return n;
+  }
 
-	bool operator ==(const Modular& o) const {
-		return n == o.n;
-	}
+  bool operator ==(const Modular& o) const {
+    return n == o.n;
+  }
 
-	bool operator !=(const Modular& o) const {
-		return n != o.n;
-	}
+  bool operator !=(const Modular& o) const {
+    return n != o.n;
+  }
 
-	Modular& operator +=(const Modular& o) {
-		n += o.n;
-		n = (n < mod ? n : n - mod);
-		return *this; 
-	}
+  Modular& operator +=(const Modular& o) {
+    n += o.n;
+    n = (n < mod ? n : n - mod);
+    return *this; 
+  }
 
-	Modular& operator -=(const Modular& o) {
-		n += mod - o.n;
-		n = (n < mod ? n : n - mod);
-		return *this;
-	}
+  Modular& operator -=(const Modular& o) {
+    n += mod - o.n;
+    n = (n < mod ? n : n - mod);
+    return *this;
+  }
 
-	Modular& operator *=(const Modular& o) {
-		n = uint64_t(n) * o.n % mod;
-		return *this;
-	}
+  Modular& operator *=(const Modular& o) {
+    n = uint64_t(n) * o.n % mod;
+    return *this;
+  }
 
-	Modular& operator /=(const Modular& o) {
-		return (*this) *= o.inv();
-	}
+  Modular& operator /=(const Modular& o) {
+    return (*this) *= o.inv();
+  }
 
-	Modular operator +(const Modular& o) const {
-		return Modular(*this) += o;
-	}
+  Modular operator +(const Modular& o) const {
+    return Modular(*this) += o;
+  }
 
-	Modular operator -(const Modular& o) const {
-		return Modular(*this) -= o;
-	}
+  Modular operator -(const Modular& o) const {
+    return Modular(*this) -= o;
+  }
 
-	Modular operator *(const Modular& o) const {
-		return Modular(*this) *= o;
-	}
+  Modular operator *(const Modular& o) const {
+    return Modular(*this) *= o;
+  }
 
-	Modular operator /(const Modular& o) const {
-		return Modular(*this) /= o;
-	}
+  Modular operator /(const Modular& o) const {
+    return Modular(*this) /= o;
+  }
 
-	Modular pow(uint64_t b) const {
-		Modular ans(1), m = Modular(*this);
-		while (b) {
-			if (b & 1) {
-				ans *= m;
-			}
-			m *= m;
-			b >>= 1;
-		}
-		return ans;
-	}
+  Modular pow(uint64_t b) const {
+    Modular ans(1), m = Modular(*this);
+    while (b) {
+      if (b & 1) {
+        ans *= m;
+      }
+      m *= m;
+      b >>= 1;
+    }
+    return ans;
+  }
 
-	Modular inv() const {
-		uint32_t a = n, b = mod, u = 0, v = 1;
-		while (a) {
-			uint32_t t = b / a;
-			b -= t * a;
-			swap(a, b);
-			u -= t * v;
-			swap(u, v);
-		}
-		assert(b == 1);
-		return Modular(u);
-	}
+  Modular inv() const {
+    uint32_t a = n, b = mod, u = 0, v = 1;
+    while (a) {
+      uint32_t t = b / a;
+      b -= t * a;
+      swap(a, b);
+      u -= t * v;
+      swap(u, v);
+    }
+    assert(b == 1);
+    return Modular(u);
+  }
 };
