d26a415b9c904684367e561ce78c18baf4202e40
Chirag Thakur <chirag11032000@gmail.com>
2019-07-07 21:30:04 +0530

    Added ABC-133

---
+++ Atcoder/ABC-133/c.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string to_string(string s) {
+  return '"' + s + '"';
+}
+
+string to_string(const char* s) {
+  return to_string((string) s);
+}
+
+string to_string(bool b) {
+  return (b ? "true" : "false");
+}
+
+template <typename A, typename B>
+string to_string(pair<A, B> p) {
+  return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+
+template <typename A>
+string to_string(A v) {
+  bool first = true;
+  string res = "{";
+  for (const auto &x : v) {
+    if (!first) {
+      res += ", ";
+    }
+    first = false;
+    res += to_string(x);
+  }
+  res += "}";
+  return res;
+}
+
+void debug_out() { cerr << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+  cerr << " " << to_string(H);
+  debug_out(T...);
+}
+
+#ifdef LOCAL
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+void test_case() {
+  int l, r;
+  cin >> l >> r;
+  if (r - l + 1 >= 2019) {
+    cout << "0\n";
+  } else {
+    int best = INT_MAX;
+    for (int i = l; i <= r; ++i) {
+      for (int j = i + 1; j <= r; ++j) {
+        int curr = (1LL * i * j) % 2019;
+        best = min(best, curr);
+      }
+    }
+    cout << best << "\n";
+  }
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  test_case();
+  return 0;
+}
