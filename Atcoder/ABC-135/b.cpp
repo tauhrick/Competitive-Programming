7225b9c5d43a0a9a435dcd1797c341ffc402b436
chirag11032000 <chirag11032000@gmail.com>
2019-07-29 17:31:32 +0530

    Upsolved ABC-135

---
+++ Atcoder/ABC-135/b.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string to_string(string s) {
+  return '"' + s + '"';
+}
+
+string to_string(const char* s) {
+  return to_string((string) s);
+}
+
+string to_string(bool b) {
+  return (b ? "true" : "false");
+}
+
+template <typename A, typename B>
+string to_string(pair<A, B> p) {
+  return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+
+template <typename A>
+string to_string(A v) {
+  bool first = true;
+  string res = "{";
+  for (const auto &x : v) {
+    if (!first) {
+      res += ", ";
+    }
+    first = false;
+    res += to_string(x);
+  }
+  res += "}";
+  return res;
+}
+
+void debug_out() { cerr << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+  cerr << " " << to_string(H);
+  debug_out(T...);
+}
+
+#ifdef LOCAL
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+void test_case() {
+  int n;
+  cin >> n;
+  vector<int> P(n + 1);
+  int bad = 0;
+  for (int i = 1; i <= n; ++i) {
+    cin >> P[i];
+    if (P[i] != i) {
+      ++bad;
+    }
+  }
+  if (bad <= 2) {
+    cout << "YES\n";
+  } else {
+    cout << "NO\n";
+  }
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  test_case();
+  return 0;
+}
---
+++ Atcoder/ABC-135/d.cpp
@@ -0,0 +1,170 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string to_string(string s) {
+  return '"' + s + '"';
+}
+
+string to_string(const char* s) {
+  return to_string((string) s);
+}
+
+string to_string(bool b) {
+  return (b ? "true" : "false");
+}
+
+template <typename A, typename B>
+string to_string(pair<A, B> p) {
+  return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+
+template <typename A>
+string to_string(A v) {
+  bool first = true;
+  string res = "{";
+  for (const auto &x : v) {
+    if (!first) {
+      res += ", ";
+    }
+    first = false;
+    res += to_string(x);
+  }
+  res += "}";
+  return res;
+}
+
+void debug_out() { cerr << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+  cerr << " " << to_string(H);
+  debug_out(T...);
+}
+
+#ifdef LOCAL
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+template <uint32_t mod>
+class Modular {
+private:
+  uint32_t n;
+
+public:
+  Modular(int64_t _n = 0) : n((_n >= 0 ? _n : mod - (-_n) % mod) % mod) {}
+
+  uint32_t get() const {
+    return n;
+  }
+
+  bool operator ==(const Modular& o) const {
+    return n == o.n;
+  }
+
+  bool operator !=(const Modular& o) const {
+    return n != o.n;
+  }
+
+  Modular& operator +=(const Modular& o) {
+    n += o.n;
+    n = (n < mod ? n : n - mod);
+    return *this; 
+  }
+
+  Modular& operator -=(const Modular& o) {
+    n += mod - o.n;
+    n = (n < mod ? n : n - mod);
+    return *this;
+  }
+
+  Modular& operator *=(const Modular& o) {
+    n = uint64_t(n) * o.n % mod;
+    return *this;
+  }
+
+  Modular& operator /=(const Modular& o) {
+    return (*this) *= o.inv();
+  }
+
+  Modular operator +(const Modular& o) const {
+    return Modular(*this) += o;
+  }
+
+  Modular operator -(const Modular& o) const {
+    return Modular(*this) -= o;
+  }
+
+  Modular operator *(const Modular& o) const {
+    return Modular(*this) *= o;
+  }
+
+  Modular operator /(const Modular& o) const {
+    return Modular(*this) /= o;
+  }
+
+  Modular pow(uint64_t b) const {
+    Modular ans(1), m = Modular(*this);
+    while (b) {
+      if (b & 1) {
+        ans *= m;
+      }
+      m *= m;
+      b >>= 1;
+    }
+    return ans;
+  }
+
+  Modular inv() const {
+    int32_t a = n, b = mod, u = 0, v = 1;
+    while (a) {
+      int32_t t = b / a;
+      b -= t * a;
+      swap(a, b);
+      u -= t * v;
+      swap(u, v);
+    }
+    assert(b == 1);
+    return Modular(u);
+  }
+};
+
+using Mint = Modular<int(1e9 + 7)>;
+using Mint_13 = Modular<13>;
+
+void test_case() {
+  string s;
+  cin >> s;
+  reverse(s.begin(), s.end());
+  vector<vector<Mint>> dp((int) s.size(), vector<Mint>(13));
+  vector<vector<int>> vis((int) s.size(), vector<int>(13));
+  function<Mint(int, Mint_13)> solve = [&](int ind, Mint_13 rem) {
+    if (ind == (int) s.size()) {
+      return (rem == 5 ? Mint(1) : Mint(0));
+    }
+    auto& ans = dp[ind][rem.get()];
+    auto& seen = vis[ind][rem.get()];
+    if (!seen) {
+      seen = true;
+      ans = 0;
+      if (s[ind] == '?') {
+        for (int curr = 0; curr <= 9; ++curr) {
+          ans += solve(ind + 1, rem + Mint_13(10).pow(ind) * Mint_13(curr));
+        }
+      } else {
+        ans += solve(ind + 1, rem + Mint_13(10).pow(ind) * Mint_13(s[ind] - '0'));
+      }
+    }
+    return ans;
+  };
+  cout << solve(0, 0).get() << "\n";
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  test_case();
+  return 0;
+}
