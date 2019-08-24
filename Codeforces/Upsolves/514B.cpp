347b274cf13de2a0b6fdaa01e224710e1bef09d4
chirag11032000 <chirag11032000@gmail.com>
2019-08-24 19:16:18 +0530

    Added speed upsolves

---
+++ Codeforces/Upsolves/514B.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string to_string(string s) {
+  return '"' + s + '"';
+}
+
+string to_string(const char* s) {
+  return to_string((string) s);
+}
+
+string to_string(bool b) {
+  return (b ? "true" : "false");
+}
+
+template <typename A, typename B>
+string to_string(pair<A, B> p) {
+  return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+
+template <typename A>
+string to_string(A v) {
+  bool first = true;
+  string res = "{";
+  for (const auto &x : v) {
+    if (!first) {
+      res += ", ";
+    }
+    first = false;
+    res += to_string(x);
+  }
+  res += "}";
+  return res;
+}
+
+void debug_out() { cerr << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+  cerr << " " << to_string(H);
+  debug_out(T...);
+}
+
+#ifdef LOCAL
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+void test_case() {
+  int n, x0, y0;
+  cin >> n >> x0 >> y0;
+  set<pair<int, int>> unq;
+  for (int i = 0; i < n; ++i) {
+    int x, y;
+    cin >> x >> y;
+    int num = y - y0;
+    int den = x - x0;
+    if (den == 0) {
+      num = 1;
+    } else if (num == 0) {
+      den = 1;
+    } else {
+      int g = __gcd(num, den);
+      num /= g;
+      den /= g;
+    }
+    unq.insert({num, den});
+  }
+  cout << unq.size() << "\n";
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  test_case();
+  return 0;
+}
