bf67e58a81ada38b620870c45f344625aff44f99
Chirag Thakur <chirag11032000@gmail.com>
2019-10-29 16:54:40 +0530

    Improved struct Matrix

--- Library/Matrix.cpp
+++ Library/Matrix.cpp
@@ -1,11 +1,12 @@
+template <typename T>
 struct Matrix {
-  vector<vector<int>> arr;
+  vector<vector<T>> arr;
   int rows, cols;
 
   Matrix(int n, int m) {
     rows = n;
     cols = m;
-    arr.assign(n, vector<int>(m));
+    arr.assign(n, vector<T>(m));
   }
 
   void make_identical() {
@@ -20,28 +21,12 @@ struct Matrix {
     }
   }
 
-  inline int add(int a, int b) {
-    int c = a + b;
-    if (c >= MOD) {
-      c -= MOD;
-    }
-    return c;
-  }
-
-  inline int mul(int a, int b) {
-    long long c = 1LL * a * b;
-    if (c >= MOD) {
-      c %= MOD;
-    }
-    return c;
-  }
-
   Matrix operator +(const Matrix &o) {
     assert(rows == o.rows && cols == o.cols);
     Matrix res(rows, cols);
     for (int i = 0; i < rows; ++i) {
       for (int j = 0; j < cols; ++j) {
-        res.arr[i][j] = add(arr[i][j], o.arr[i][j]);
+        res.arr[i][j] = arr[i][j] + o.arr[i][j];
       }
     }
     return res;
@@ -53,7 +38,7 @@ struct Matrix {
     for (int i = 0; i < rows; ++i) {
       for (int j = 0; j < o.cols; ++j) {
         for (int k = 0; k < cols; ++k) {
-          res.arr[i][j] = add(res.arr[i][j], mul(arr[i][k], o.arr[k][j]));
+          res.arr[i][j] += arr[i][k] * o.arr[k][j];
         }
       }
     }
