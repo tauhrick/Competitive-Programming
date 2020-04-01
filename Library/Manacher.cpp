52afcfb686cc084026baf18dfbb077ac26931f5d
Chirag Thakur <chirag11032000@gmail.com>
2020-04-01 23:20:29 +0530

    fixed bug

--- Library/Manacher.cpp
+++ Library/Manacher.cpp
@@ -1,18 +1,22 @@
 template <typename A>
 vector<int> manacher(A v, int st, int en, int parity) {
+    // In case of even parity, [-- i (i + 1) --]
+    // i denotes imaginary point b/w i and i + 1
     int rev_parity = parity ^ 1;
     vector<int> d(v.size(), -1);
     for (int i = st, l = st, r = st - 1; i <= en; ++i) {
-        int ans = (i <= r) ? min(d[l + r - i - rev_parity], r - i + parity) : parity;
+        int ans = (i <= r - rev_parity) ? min(d[l + r - i - rev_parity], r - i + parity) : parity;
         int curr_l = i - ans + 1;
         int curr_r = i + ans - parity;
         while (st <= curr_l - 1 && curr_r + 1 <= en && v[curr_l - 1] == v[curr_r + 1]) {
             ++ans, --curr_l, ++curr_r;
         }
         d[i] = ans;
-        if (curr_r > r) {
-            l = curr_l, r = curr_r;
+        if (curr_l <= curr_r && curr_r > r) {
+            l = curr_l;
+            r = curr_r;
         }
     }
     return d;
 }
+
