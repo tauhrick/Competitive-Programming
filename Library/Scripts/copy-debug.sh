00ff789f47476e76d69c63a08546c9196040368f
Chirag Thakur <chirag11032000@gmail.com>
2021-01-10 09:58:40 +0530

    Adds script to copy Debug.hpp to /usr/include/local and precompile it

---
+++ Library/Scripts/copy-debug.sh
@@ -0,0 +1,2 @@
+cp ../Debug.hpp /usr/local/include
+g++ -Wall -Wextra -pedantic -std=c++17 -O2 -Wshadow -Wformat=2 -Wfloat-equal -Wconversion -Wlogical-op -Wshift-overflow=2 -Wduplicated-cond -Wcast-qual -Wcast-align -Wno-unused-result -Wno-sign-conversion -D_GLIBCXX_DEBUG -DLOCAL -D_GLIBCXX_DEBUG_PEDANTIC -D_FORTIFY_SOURCE=2 -fsanitize=address -fsanitize=undefined -fno-sanitize-recover=all -fstack-protector /usr/local/include/Debug.hpp
