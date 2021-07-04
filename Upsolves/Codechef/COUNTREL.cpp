82a982f581ad36a3b85e722537707950d3a642c1
Chirag Thakur <chirag11032000@gmail.com>
2021-07-04 15:32:09 +0530

    Adds practice problem for inclusion-exclusion principle

---
+++ Upsolves/Codechef/COUNTREL.cpp
@@ -0,0 +1,102 @@
+#ifndef LOCAL
+#include <bits/stdc++.h>
+using namespace std;
+#define debug(...) 42
+#else
+#include "Debug.hpp"
+#endif
+
+template <uint32_t mod>
+class Modular {
+private:
+  uint32_t n;
+
+public:
+  Modular(int64_t _n = 0) : n(uint32_t((_n >= 0 ? _n : mod - (-_n) % mod) % mod)) {}
+
+  uint32_t get() const { return n; }
+  bool operator==(const Modular &o) const { return n == o.n; }
+  bool operator!=(const Modular &o) const { return n != o.n; }
+  Modular& operator/=(const Modular &o) { return (*this) *= o.inv(); }
+  Modular operator+(const Modular &o) const { return Modular(*this) += o; }
+  Modular operator-(const Modular &o) const { return Modular(*this) -= o; }
+  Modular operator*(const Modular &o) const { return Modular(*this) *= o; }
+  Modular operator/(const Modular &o) const { return Modular(*this) /= o; }
+  friend string to_string(const Modular &m) { return to_string(m.get()); }
+
+  Modular& operator+=(const Modular &o) {
+    n += o.n;
+    n = (n < mod ? n : n - mod);
+    return *this; 
+  }
+
+  Modular& operator-=(const Modular &o) {
+    n += mod - o.n;
+    n = (n < mod ? n : n - mod);
+    return *this;
+  }
+
+  Modular& operator*=(const Modular &o) {
+    n = uint32_t(uint64_t(n) * o.n % mod);
+    return *this;
+  }
+
+  Modular pow(uint64_t b) const {
+    Modular ans(1), m = Modular(*this);
+    while (b) {
+      if (b & 1) {
+        ans *= m;
+      }
+      m *= m;
+      b >>= 1;
+    }
+    return ans;
+  }
+
+  Modular inv() const {
+    int32_t a = n, b = mod, u = 0, v = 1;
+    while (a) {
+      int32_t t = b / a;
+      b -= t * a;
+      swap(a, b);
+      u -= t * v;
+      swap(u, v);
+    }
+    assert(b == 1);
+    return Modular(u);
+  }
+};
+
+class Task {
+ public:
+  void Perform() {
+    Read();
+    Solve();
+  }
+ 
+ private:
+  using Mint = Modular<int(1.0e8) + 7>;
+
+  int64_t n;  
+
+  void Read() {
+    cin >> n; 
+  }
+ 
+  void Solve() {
+    Mint r1 = (Mint(3).pow(n) - Mint(2).pow(n + 1) + 1) / Mint(2);
+    Mint r2 = ((Mint(2).pow(2 * n) + Mint(2).pow(n) - Mint(2) * Mint(3).pow(n)) / Mint(2)) - r1;
+    cout << r1.get() << ' ' << r2.get() << '\n';
+  }
+};
+ 
+int main() {
+  ios_base::sync_with_stdio(false), cin.tie(nullptr);
+  int tc;
+  cin >> tc;
+  while (tc--) {
+    Task t;
+    t.Perform();
+  }
+  return 0;
+}
